9968f944e614c97fd24c83516c24680ef6a49ac3
richardh888 <richardh888@hotmail.com>
2013-04-29 14:52:37 -0400

    initial commit

---
+++ Models/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mvc4Sample.Web.Models
+{
+    public class Person
+    {
+        public int PersonID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
